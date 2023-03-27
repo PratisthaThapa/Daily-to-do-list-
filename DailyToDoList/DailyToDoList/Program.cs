74b4b8c521ca7033d422d72c476926c86bedb25c
Pratistha Thapa <thapapratistha2@gmail.com>
2023-03-26 20:15:22 -0400

    Add tasks logic in program file

--- DailyToDoList/DailyToDoList/Program.cs
+++ DailyToDoList/DailyToDoList/Program.cs
@@ -1,2 +1,87 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+class Task
+{
+    public string Description { get; set; }
+    public bool IsComplete { get; set; }
+}
+class TodoList : List<Task>
+{
+    public void AddTask(Task task)
+    {
+        this.Add(task);
+    }
+
+    public void DisplayTasks()
+    {
+        Console.WriteLine("\n Printing all tasks:");
+
+        foreach (Task task in this)
+        {
+            string taskCompeted = " ";
+            if (task.IsComplete)
+            {
+                taskCompeted = "X";
+            }
+
+            Console.WriteLine("{0} [{1}]", task.Description, taskCompeted);
+        }
+    }
+
+    public void DisplayCompletedTasks()
+    {
+        var completedTasks = from task in this
+                             where task.IsComplete == true
+                             select task;
+        Console.WriteLine("\nPrinting completed tasks:");
+
+        foreach (Task task in completedTasks)
+        {
+
+            Console.WriteLine("{0} [{1}]", task.Description, task.IsComplete ? "X" : " ");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TodoList todoList = new TodoList();
+
+        // Adding tasks to the list
+        Task task1 = new Task { Description = "Meeting", IsComplete = false };
+        Task task2 = new Task { Description = "Work out", IsComplete = true };
+        Task task3 = new Task { Description = "Finish project", IsComplete = false };
+        todoList.AddTask(task1);
+        todoList.AddTask(task2);
+        todoList.AddTask(task3);
+
+        // Marking task 1 as complete
+        //todoList[0].IsComplete = true;
+        //todoList.DisplayTasks();
+
+        // Displaying completed tasks
+        //todoList.DisplayCompletedTasks();
+
+        //in a while loop
+        //print type 1 to Add tasks, type 2 to mark task completed, type 3 to display all tasks, type 4 to display completed tasks, type 5 to quit -> break while loop
+        //if 1
+        //print enter task description
+        //take user's input         Console.ReadLine();
+        // mark isComplete = false
+        // add the task to todoList
+        //if 2
+        //ask user for description of which task to mark complete
+        // lowercase user's input and task descriptions from todoList and see if they match
+        //if match
+        //mark task IsComplete = true
+
+
+
+
+
+    }
+}
---
+++ DailyToDoList/DailyToDoList/TasksData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DailyToDoList
+{
+    public class TasksData
+    {
+
+
+
+
+        public void ReadTasks ()
+        {
+
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                //List<dict> items = JsonConvert.DeserializeObject<List<dict>>(json);
+            }
+        }
+
+	}
+
+}
+
